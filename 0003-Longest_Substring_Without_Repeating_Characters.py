7308d65c5dbff2699a201960dd6743b43e866e1f
Shin Ikeji <shin@jay7.net>
2020-01-01 23:24:29 -0800

    Fix edge cases

--- 0003-Longest_Substring_Without_Repeating_Characters.py
+++ 0003-Longest_Substring_Without_Repeating_Characters.py
@@ -6,31 +6,32 @@ class Solution:
         max_ = 0
         for i in range(len(s)):
             unique_list = [s[i]]
+            max_ = max(max_, len(unique_list))
             for j in range(i+1, len(s)):
                 if s[j] not in unique_list:
                     unique_list.append(s[j])
+                    max_ = max(max_, len(unique_list))
                 else:
                     max_ = max(max_, len(unique_list))
+                    break
 
         return max_
+
+
+test = Solution()
+test.lengthOfLongestSubstring("abcabcbb") # 3
+# test.lengthOfLongestSubstring("bbbbb") # 1
+# test.lengthOfLongestSubstring("pwwkew") # 3
+# test.lengthOfLongestSubstring(" ") # 1
+# test.lengthOfLongestSubstring("") # 0
+# test.lengthOfLongestSubstring("au") # 2
 
 
 # import timeit
 
 # code = """
-# class Solution:
-#     def lengthOfLongestSubstring(self, s) -> int:
-#         max_ = 0
-#         for i in range(len(s)):
-#             unique_list = [s[i]]
-#             for j in range(i+1, len(s)):
-#                 if s[j] not in unique_list:
-#                     unique_list.append(s[j])
-#                 else:
-#                     max_ = max(max_, len(unique_list))
-
-#         return max_
-
+#
+#
 # test = Solution()
 # test.lengthOfLongestSubstring("abcabcbb")
 # """
