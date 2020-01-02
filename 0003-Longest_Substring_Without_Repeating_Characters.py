04dd326436e504e9b7e2bd927e3eb967a708c1be
Shin Ikeji <shin@jay7.net>
2020-01-02 13:15:02 -0800

    Sliding window

--- 0003-Longest_Substring_Without_Repeating_Characters.py
+++ 0003-Longest_Substring_Without_Repeating_Characters.py
@@ -1,5 +1,6 @@
 # original: 0.118911507
 # Brute force: 0.617322538
+# Sliding window: 0.162572
 
 from __future__ import annotations
 
@@ -7,17 +8,18 @@ from __future__ import annotations
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
         n = len(s)
-        max_ = 0
-        for i in range(n):
-            for j in range(i+1, n+1):
-                if allUnique(s, i, j):
-                    max_ = max(max_, j-i)
+        unique_list = []
+        max_, i, j = 0, 0, 0
+        while (i < n and j < n):
+            if s[j] not in unique_list:
+                unique_list.append(s[j])
+                max_ = max(max_, j-i+1)
+                j += 1
+            else:
+                unique_list = unique_list[1:]
+                i += 1
 
         return max_
-
-def allUnique(s: str, start: int, end: int) -> bool:
-    range_ = s[start:end]
-    return True if len(range_) == len(set(range_)) else False
 
 
 test = Solution()
@@ -35,17 +37,18 @@ test.lengthOfLongestSubstring("au") # 2
 # class Solution:
 #     def lengthOfLongestSubstring(self, s: str) -> int:
 #         n = len(s)
-#         max_ = 0
-#         for i in range(n):
-#             for j in range(i+1, n+1):
-#                 if allUnique(s, i, j):
-#                     max_ = max(max_, j-i)
+#         unique_list = []
+#         max_, i, j = 0, 0, 0
+#         while (i < n and j < n):
+#             if s[j] not in unique_list:
+#                 unique_list.append(s[j])
+#                 max_ = max(max_, j-i+1)
+#                 j += 1
+#             else:
+#                 unique_list = unique_list[1::]
+#                 i += 1
 
 #         return max_
-
-# def allUnique(s: str, start: int, end: int) -> bool:
-#     range_ = s[start:end]
-#     return True if len(range_) == len(set(range_)) else False
 
 # test = Solution()
 # test.lengthOfLongestSubstring("abcabcbb") # 3
