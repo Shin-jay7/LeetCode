26b5aa9a0c9d8dce8b1a5a74bd443803696491ab
Shin Ikeji <shin@jay7.net>
2020-01-03 19:47:14 -0800

    First stab

---
+++ 0004-Median_of_Two_Sorted_Arrays.py
@@ -0,0 +1,20 @@
+from __future__ import annotations
+
+
+class Solution:
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        L = sorted(nums1+nums2)
+        odd, mid = len(L)%2, len(L)//2
+        return float(L[mid]) if odd else (L[mid-1]+L[mid])/2
+        # print(float(L[mid])) if odd else print((L[mid-1]+L[mid])/2)
+
+
+test = Solution()
+nums1 = [1, 3]
+nums2 = [2]
+test.findMedianSortedArrays(nums1, nums2) # 2.0
+
+test = Solution()
+nums1 = [1, 2]
+nums2 = [3, 4]
+test.findMedianSortedArrays(nums1, nums2) # 2.5
