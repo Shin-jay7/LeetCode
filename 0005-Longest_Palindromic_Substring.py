bc3106a52d222ca0f80010e831c96629b4cea5e2
Shin Ikeji <shin@jay7.net>
2020-01-04 16:03:59 -0800

    ozarwork's approach: easy to understand

--- 0005-Longest_Palindromic_Substring.py
+++ 0005-Longest_Palindromic_Substring.py
@@ -3,25 +3,18 @@ from __future__ import annotations
 
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        start, end = 0, 0
+        longest = ''
 
         for i in range(len(s)):
-            length1 = expandAroundCenter(s, i, i)
-            length2 = expandAroundCenter(s, i, i+1)
-            length = max(length1, length2)
-            if length > end-start:
-                start = i - (length-1)//2
-                end = i + length//2
+            for j in range(len(s), i, -1):
+                if len(longest) >= j-i:
+                    break
+                elif s[i:j] == s[i:j][::-1]:
+                    longest = s[i:j]
+                    break
 
-        # return s[start:end+1]
-        print(s[start:end+1])
-
-def expandAroundCenter(s: str, L: int, R: int) -> int:
-    while L >=0 and R < len(s) and s[L] == s[R]:
-        L -= 1
-        R += 1
-
-    return R - L - 1
+        return longest
+        # print(longest)
 
 
 test = Solution()
