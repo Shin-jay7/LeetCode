e57914618bd8be17c8c785d2321e17ee7a8ce20f
Shin Ikeji <shin@jay7.net>
2020-01-04 17:26:07 -0800

    Return to Expand approach as it's faster

--- 0005-Longest_Palindromic_Substring.py
+++ 0005-Longest_Palindromic_Substring.py
@@ -3,18 +3,25 @@ from __future__ import annotations
 
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        longest = ''
+        start, end = 0, 0
 
         for i in range(len(s)):
-            for j in range(len(s), i, -1):
-                if len(longest) >= j-i:
-                    break
-                elif s[i:j] == s[i:j][::-1]:
-                    longest = s[i:j]
-                    break
+            length1 = expandAroundCenter(s, i, i)
+            length2 = expandAroundCenter(s, i, i+1)
+            length = max(length1, length2)
+            if length > end-start:
+                start = i - (length-1)//2
+                end = i + length//2
 
-        return longest
-        # print(longest)
+        return s[start:end+1]
+
+
+def expandAroundCenter(s: str, L: int, R: int) -> int:
+    while L >=0 and R < len(s) and s[L] == s[R]:
+        L -= 1
+        R += 1
+
+    return R - L - 1
 
 
 test = Solution()
