e139f5f3d4b8fd62b6d9487b2ed4bbd2216079d3
Shin Ikeji <shin@jay7.net>
2020-01-10 13:01:12 -0800

    Revert wrong fix

--- 0005-Longest_Palindromic_Substring.py
+++ 0005-Longest_Palindromic_Substring.py
@@ -14,7 +14,7 @@ class Solution:
             ans = s[i]
 
         max_ = 1
-        for i in range(n, -1, -1):
+        for i in range(n-1, -1, -1):
             for j in range(i+1, n):
                 if s[i] == s[j]:
                     if j-i == 1 or dp[i+1][j-1]:
