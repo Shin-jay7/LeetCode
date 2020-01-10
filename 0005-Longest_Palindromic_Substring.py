956cea9e5093a5e02829720c045e69a8eda50dc6
Shin Ikeji <shin@jay7.net>
2020-01-10 12:07:51 -0800

    DP solution, but too slow..

--- 0005-Longest_Palindromic_Substring.py
+++ 0005-Longest_Palindromic_Substring.py
@@ -3,25 +3,33 @@ from __future__ import annotations
 
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        start, end = 0, 0
+        if s == "" or s == " ":
+            return ""
 
-        for i in range(len(s)):
-            length1 = expandAroundCenter(s, i, i)
-            length2 = expandAroundCenter(s, i, i+1)
-            length = max(length1, length2)
-            if length > end-start:
-                start = i - (length-1)//2
-                end = i + length//2
+        n = len(s)
 
-        return s[start:end+1]
+        L = [[[0, ""] for x in range(n)] for x in range(n)]
 
+        for i in range(n):
+            L[i][i][0] = 1
+            L[i][i][1] = s[i]
 
-def expandAroundCenter(s: str, L: int, R: int) -> int:
-    while L >=0 and R < len(s) and s[L] == s[R]:
-        L -= 1
-        R += 1
+        for cl in range(2, n+1):
+            for i in range(n-cl+1):
+                j = i+cl-1
+                if s[i] == s[j] and cl == 2:
+                    L[i][j][0] = 2
+                    L[i][j][1] = s[i:j+1]
+                elif s[i] == s[j] and L[i+1][j-1][0] == cl-2:
+                    L[i][j][0] = cl
+                    L[i][j][1] = s[i:j+1]
+                else:
+                    if L[i][j-1][0] > L[i+1][j][0]:
+                        L[i][j] = L[i][j-1]
+                    else:
+                        L[i][j] = L[i+1][j]
 
-    return R - L - 1
+        return L[0][n-1][1]
 
 
 test = Solution()
@@ -41,4 +49,7 @@ test.longestPalindrome("babad") # "bab" or "aba"
 
 test = Solution()
 test.longestPalindrome("cbbd") # "bb"
+
+test = Solution()
+test.longestPalindrome("abcda") # "a"
 
