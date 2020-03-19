373fc2ed40f2176a0990fd5cd1199f7333d198c7
Shin Ikeji <shin@jay7.net>
2020-03-19 10:16:31 -0700

    One path algorithm

--- 0019-Remove_Nth_Node_from_End_of_List.py
+++ 0019-Remove_Nth_Node_from_End_of_List.py
@@ -11,21 +11,17 @@ class Solution:
     def removeNthFromEnd(self, head: ListNode, n: int) -> ListNode:
         dummy = ListNode(0)
         dummy.next = head
-        length = 0
-        first = head
+        first = dummy
+        second = dummy
+
+        for _ in range(n+1):
+            first = first.next
 
         while first != None:
-            length += 1
             first = first.next
+            second = second.next
 
-        length -= n
-        first = dummy
-
-        while length > 0:
-            length -= 1
-            first = first.next
-
-        first.next = first.next.next
+        second.next = second.next.next
 
         return dummy.next
 
