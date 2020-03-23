9ff884a4e44058798664b1880d4816cb02b3a876
Shin Ikeji <shin@jay7.net>
2020-03-23 10:22:21 -0700

    Divide and conquer approach

--- 0023-Merge_k_Sorted_Lists.py
+++ 0023-Merge_k_Sorted_Lists.py
@@ -8,20 +8,32 @@ class ListNode:
 
 class Solution:
     def mergeKLists(self, lists: List[ListNode]) -> ListNode:
-        min_heap = []
+        amount = len(lists)
+        interval = 1
+
+        while interval < amount:
+            for i in range(0, amount-interval, interval*2):
+                lists[i] = self.merge2Lists(lists[i], lists[i+interval])
+            interval *= 2
+
+        return lists[0] if amount > 0 else lists
+
+    def merge2Lists(self, l1, l2):
         head = pointer = ListNode(0)
 
-        for l in lists:
-            if l:
-                heapq.heappush(min_heap, (l.val, id(l), l))
-
-        while min_heap:
-            val, node_id, node, = heapq.heappop(min_heap)
-            pointer.next = node
+        while l1 and l2:
+            if l1.val <= l2.val:
+                pointer.next = l1
+                l1 = l1.next
+            else:
+                pointer.next = l2
+                l2 = l1
+                l1 = pointer.next.next
             pointer = pointer.next
-            node = node.next
 
-            if node:
-                heapq.heappush(min_heap, (node.val, id(node), node))
+        if not l1:
+            pointer.next = l2
+        else:
+            pointer.next = l1
 
         return head.next
