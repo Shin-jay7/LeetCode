152183bb4972b0d2f259d52adb8e07b23250ff90
Shin Ikeji <shin@jay7.net>
2020-03-25 11:01:42 -0700

    Succinct iterative approach

--- 0025-Reverse_Nodes_in_k-Group.py
+++ 0025-Reverse_Nodes_in_k-Group.py
@@ -9,40 +9,20 @@ class ListNode:
 
 class Solution:
     def reverseKGroup(self, head, k):
-        def swap(begin, end):
-            """
-            dummy->1->2->3->4->5->6->nil
-            begin        end
+        dummy = jump = ListNode()
+        dummy.next = l = r = head
 
-            =>
-
-            dummy->2->1->3->4->5->6->nil
-            begin        end
-            """
-            p = begin.next
-            q = p.next
-            while q != end:
-                tmp = q.next
-                q.next = p
-                p = q
-                q = tmp
-            begin.next.next = end
-            begin.next = p
-
-        dummy = ListNode()
-        dummy.next = head
-        prev, curr = dummy, head
-
-        while curr:
+        while True:
             count = 0
-            for _ in range(k):
-                if curr:
-                    curr = curr.next
-                    count += 1
+            while r and count < k:
+                r = r.next
+                count += 1
             if count == k:
-                swap(prev, curr)
-            while prev.next != curr:
-                prev = prev.next
-
-        return dummy.next
-
+                pre, cur = r, l
+                for _ in range(k):
+                    cur.next, cur, pre = pre, cur.next, cur
+                # jump : used to connect last node in previous k-group
+                # to first node in following k-group
+                jump.next, jump, l = pre, l, r
+            else:
+                return dummy.next
