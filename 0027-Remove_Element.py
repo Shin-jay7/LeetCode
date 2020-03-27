23e6b74e8d227cf979730098cdd20acbabb0bf97
Shin Ikeji <shin@jay7.net>
2020-03-27 09:04:34 -0700

    Reduce array size if elements are matched

--- 0027-Remove_Element.py
+++ 0027-Remove_Element.py
@@ -3,11 +3,12 @@ from __future__ import annotations
 
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
-        i, j = 0, 0
-        while j < len(nums):
-            if nums[j] != val:
-                nums[i] = nums[j]
+        i, j = 0, len(nums)
+        while i < j:
+            if nums[i] == val:
+                nums[i] = nums[j-1]
+                j -= 1
+            else:
                 i += 1
-            j += 1
 
-        return i
+        return j
