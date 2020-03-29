2118302397588c17b318609c46e5ee572e53f2c3
Shin Ikeji <shin@jay7.net>
2020-03-29 09:16:47 -0700

    Second attempt

--- 0029-Divide_Two_Integers.py
+++ 0029-Divide_Two_Integers.py
@@ -3,17 +3,16 @@ from __future__ import annotations
 
 class Solution:
     def divide(self, dividend: int, divisor: int) -> int:
-        positive = (dividend < 0) is (divisor < 0)
+        positive = (dividend < 0) == (divisor < 0)
         dividend, divisor = abs(dividend), abs(divisor)
         ans = 0
 
         while dividend >= divisor:
-            temp, i = divisor, 1
-            while dividend >= temp:
-                dividend -= temp
-                ans += i
-                i <<= 1
-                temp <<= 1
+            i = 0
+            while dividend >= divisor << (i+1):
+                i += 1
+            ans += 1 << i
+            dividend -= divisor << i
 
         if not positive:
             ans *= -1
