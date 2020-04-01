fa8fa05242ba1cd876e6105286096851d19f360b
Shin Ikeji <shin@jay7.net>
2020-04-01 14:04:23 -0700

    DP solution

--- 0032-Longest_Valid_Parentheses.py
+++ 0032-Longest_Valid_Parentheses.py
@@ -4,22 +4,17 @@ from __future__ import annotations
 class Solution:
     def longestValidParentheses(self, s: str) -> int:
         max_ = 0
+        dp = [0]*len(s)
 
-        def isValid(substr: str) -> bool:
-            stack = []
-            for i in range(len(substr)):
-                if substr[i] == "(":
-                    stack.append(substr[i])
-                elif stack != []:
-                    stack.pop()
-                else:
-                    return False
-            return stack == []
-
-        for i in range(len(s)):
-            for j in range(i+2,len(s)+1,2):
-                if isValid(s[i:j]):
-                    max_ = max(max_, j-i)
+        for i in range(1,len(s)):
+            if s[i] == ")" and s[i-1] == "(":
+                dp[i] = dp[i-2] + 2
+            elif s[i] == ")" and s[i-1] == ")":
+                idx = i-dp[i-1]-1
+                if idx >= 0 and s[idx] == "(":
+                    idx = max(0, idx-1)
+                    dp[i] = dp[idx] + dp[i-1] +  2
+            max_ = max(max_, dp[i])
 
         return max_
         # print(max_)
@@ -36,3 +31,9 @@ test.longestValidParentheses("()()") # 4
 
 test = Solution()
 test.longestValidParentheses("()(()") # 2
+
+test = Solution()
+test.longestValidParentheses("(()))())(") # 4
+
+test = Solution()
+test.longestValidParentheses("()(())") # 6
