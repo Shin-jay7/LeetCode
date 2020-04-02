b556e7c30e01189b3e1a249361d1ef55047356fc
Shin Ikeji <shin@jay7.net>
2020-04-02 09:48:55 -0700

    another stack approach

--- 0032-Longest_Valid_Parentheses.py
+++ 0032-Longest_Valid_Parentheses.py
@@ -4,17 +4,17 @@ from __future__ import annotations
 class Solution:
     def longestValidParentheses(self, s: str) -> int:
         max_ = 0
-        dp = [0]*len(s)
+        stack = [-1]
 
-        for i in range(1,len(s)):
-            if s[i] == ")" and s[i-1] == "(":
-                dp[i] = dp[i-2] + 2
-            elif s[i] == ")" and s[i-1] == ")":
-                idx = i-dp[i-1]-1
-                if idx >= 0 and s[idx] == "(":
-                    idx = max(0, idx-1)
-                    dp[i] = dp[idx] + dp[i-1] +  2
-            max_ = max(max_, dp[i])
+        for i in range(len(s)):
+            if s[i] == "(":
+                stack.append(i)
+            else:
+                stack.pop()
+                if stack == []:
+                    stack.append(i)
+                else:
+                    max_ = max(max_, i-stack[-1])
 
         return max_
         # print(max_)
