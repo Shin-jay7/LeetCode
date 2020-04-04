4e7fcc60683051b30768f4fa1bacda027016f8f5
Shin Ikeji <shin@jay7.net>
2020-04-03 20:49:04 -0700

    tweaked binary search

--- 0034-Find_First_and_Last_Position_of_Element_in_Sorted_Array.py
+++ 0034-Find_First_and_Last_Position_of_Element_in_Sorted_Array.py
@@ -2,20 +2,25 @@ from __future__ import annotations
 
 
 class Solution:
+    def insertion_idx(self, nums, target, left):
+        l, r = 0, len(nums)
+
+        while l < r:
+            mid = (l+r)//2
+            if nums[mid] > target or (left and target == nums[mid]):
+                r = mid
+            else:
+                l = mid+1
+
+        return l
+
     def searchRange(self, nums: List[int], target: int) -> List[int]:
-        for i in range(len(nums)):
-            if nums[i] == target:
-                l_idx = i
-                break
-        else:
+        l_idx = self.insertion_idx(nums, target, True)
+
+        if l_idx == len(nums) or nums[l_idx] != target:
             return [-1, -1]
 
-        for j in range(len(nums)-1, -1, -1):
-            if nums[j] == target:
-                r_idx = j
-                break
-
-        return [l_idx, r_idx]
+        return [l_idx, self.insertion_idx(nums, target, False)-1]
 
 
 test = Solution()
