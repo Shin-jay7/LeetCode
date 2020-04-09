97ab2a6b34a15248b3fea4bd5250ac8bda9f5d00
Shin Ikeji <shin@jay7.net>
2020-04-09 09:42:28 -0700

    swap and check approach

--- 0041-First_Missing_Positive.py
+++ 0041-First_Missing_Positive.py
@@ -3,15 +3,22 @@ from __future__ import annotations
 
 class Solution:
     def firstMissingPositive(self, nums: List[int]) -> int:
-        nums.sort()
-        i = 1
-        while True:
-            if i not in nums:
-                # print(i)
-                # return
-                return i
+        i, count = 0, 1
+        l = len(nums)
+
+        while i < l:
+            num = nums[i]
+            if 1 <= num < l and nums[num-1] != nums[i]:
+                nums[num-1], nums[i] = nums[i], nums[num-1]
+                # print(nums)
             else:
                 i += 1
+                # print(i)
+                while count-1 < l and nums[count-1] == count:
+                    count += 1
+                # print(count)
+
+        return count
 
 
 test = Solution()
@@ -20,5 +27,5 @@ test.firstMissingPositive([1,2,0]) # 3
 test = Solution()
 test.firstMissingPositive([3,4,-1,1]) # 2
 
-test = Solution()
-test.firstMissingPositive([7,8,9,11,12]) # 1
+# test = Solution()
+# test.firstMissingPositive([7,8,9,11,12]) # 1
