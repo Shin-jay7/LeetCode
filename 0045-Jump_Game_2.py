d3aa415c3d22046081fc386b810514b863d9c2a3
Shin Ikeji <shin@jay7.net>
2020-04-14 09:08:13 -0700

    second attempt

--- 0045-Jump_Game_2.py
+++ 0045-Jump_Game_2.py
@@ -3,12 +3,14 @@ from __future__ import annotations
 
 class Solution:
     def jump(self, nums: List[int]) -> int:
-        start, end, max_, step = 0, 0, 0, 0
+        if len(nums) <= 1: return 0
+
+        start, end, step, max_ = 0, nums[0], 1, 0
         while end < len(nums)-1:
             step += 1
             for i in range(start, end+1):
                 max_ = max(max_, nums[i]+i)
-            start, end = end+1, max_
+            start, end = end, max_
 
         return step
 
