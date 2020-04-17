17b9ca6eb3521a4e3acd66f99bd0f7173fd68368
Shin Ikeji <shin@jay7.net>
2020-04-17 09:57:28 -0700

    first attempt

---
+++ 0046-Permutations.py
@@ -0,0 +1,29 @@
+from __future__ import annotations
+
+
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        ans = []
+        self.dfs(nums, [], ans)
+        # print(ans)
+        return ans
+
+    def dfs(self, nums, perm, ans):
+        if not nums:
+            ans.append(perm)
+        for i in range(len(nums)):
+            self.dfs(nums[:i]+nums[i+1:], perm+[nums[i]], ans)
+
+
+test = Solution()
+test.permute([1,2,3])
+"""
+  [
+  [1,2,3],
+  [1,3,2],
+  [2,1,3],
+  [2,3,1],
+  [3,1,2],
+  [3,2,1]
+]
+"""
