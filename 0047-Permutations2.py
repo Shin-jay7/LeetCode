bf78833987c7e4906c83d4a8f5040dfacf58a774
Shin Ikeji <shin@jay7.net>
2020-04-17 19:48:55 -0700

    revise comment

--- 0047-Permutations2.py
+++ 0047-Permutations2.py
@@ -1,12 +1,5 @@
 from __future__ import annotations
 
-"""
-either the new element does not exist in rest (in this case just insert it
-everywhere like before) or we only insert up to the first occurrence
-rest.index(head), because any other slot after the first occurrence can be
-thought as the duplicate of the first occurrence as the inserted element
-and the slot being the existing one.
-"""
 
 class Solution:
     def permuteUnique(self, nums: List[int]) -> List[List[int]]:
@@ -16,14 +9,16 @@ class Solution:
             # print("n: "+str(n))
             perms = []
             for l in ans:
-                # print("** second loop begins")
+                # print("** second loop begins **")
                 # print("l: "+str(l))
                 # print("(l+[n]).index(n)+1: "+str((l+[n]).index(n)+1))
+                #               ↓ this trick skips duplicate case
                 for i in range((l+[n]).index(n)+1):
                     # print("** third loop begins")
                     # print("i: "+str(i))
                     # print("l[:i]+[n]+l[i:]: "+str(l[:i]+[n]+l[i:]))
                     perms.append(l[:i]+[n]+l[i:])
+                    # print("perms: "+str(perms))
             #         print("** third loop ends**")
             #     print("** second loop ends**")
             # print("** first loop ends**")
