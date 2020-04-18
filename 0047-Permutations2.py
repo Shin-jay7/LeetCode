8b9a3a44d43c56dba9acae8c4bd89adeb08e693c
Shin Ikeji <shin@jay7.net>
2020-04-17 17:47:18 -0700

    third attempt with reference url

--- 0047-Permutations2.py
+++ 0047-Permutations2.py
@@ -1,17 +1,25 @@
 from __future__ import annotations
 from collections import Counter
 
-
 class Solution:
     def permuteUnique(self, nums: List[int]) -> List[List[int]]:
-        ans = [[]]
-        for n in nums:
-            perms = []
-            for l in ans:
-                for i in range((l+[n]).index(n)+1):
-                    perms.append(l[:i]+[n]+l[i:])
-            ans = perms
+        ans = []
 
+        def btrack(perm, counter):
+            if len(perm)==len(nums):
+                ans.append(perm[:])
+                # https://stackoverflow.com/questions/32448414/what-does-colon-at-assignment-for-list-do-in-python
+            for i in counter:
+                if counter[i] > 0:
+                    perm.append(i)
+                    counter[i] -= 1
+                    btrack(perm, counter)
+                    perm.pop()
+                    counter[i] += 1
+
+        btrack([], Counter(nums))
+
+        # print(ans)
         return ans
 
 
