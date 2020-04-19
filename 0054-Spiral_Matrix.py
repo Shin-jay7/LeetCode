bbb94562296d9f95410f4592e6a3029fc9da1728
Shin Ikeji <shin@jay7.net>
2020-04-19 00:01:55 -0700

    Second attempt

--- 0054-Spiral_Matrix.py
+++ 0054-Spiral_Matrix.py
@@ -3,14 +3,24 @@ from __future__ import annotations
 
 class Solution:
     def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
+        if not matrix: return []
+        M, N = len(matrix), len(matrix[0])
+        seen = [[False]*N for _ in matrix]
         ans = []
+        direc_m = [0, 1, 0, -1]
+        direc_n = [1, 0, -1, 0]
+        m, n, idx = 0, 0, 0
 
-        while matrix:
-            # pop & clockwise rotation, pop & ...
-            ans += matrix.pop(0)
-            matrix = [*zip(*matrix)][::-1]
+        for _ in range(M*N):
+            ans.append(matrix[m][n])
+            seen[m][n] = True
+            next_m, next_n = m+direc_m[idx], n+direc_n[idx]
+            if 0 <= next_m < M and 0 <= next_n < N and not seen[next_m][next_n]:
+                m, n = next_m, next_n
+            else:
+                idx = (idx+1)%4
+                m, n = m+direc_m[idx], n+direc_n[idx]
 
-        # print(ans)
         return ans
 
 
