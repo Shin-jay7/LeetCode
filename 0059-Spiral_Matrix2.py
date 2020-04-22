6810da846c8ac0167abdd4492373e210cdf8f267
Shin Ikeji <shin@jay7.net>
2020-04-22 11:40:05 -0700

    third attempt

--- 0059-Spiral_Matrix2.py
+++ 0059-Spiral_Matrix2.py
@@ -3,29 +3,17 @@ from __future__ import annotations
 
 class Solution:
     def generateMatrix(self, n: int) -> List[List[int]]:
-        ans = [[""]*n for _ in range(n)]
-        left, right, top, down, num = 0, n-1, 0, n-1, 1
+        """
+        Start with the empty matrix, add the numbers in reverse order
+        until we added the number 1. Always rotate the matrix clockwise
+        and add a top row
+        """
+        ans, low = [], n*n+1
 
-        while left <= right and top <= down:
-            for _ in range(left, right+1):
-                ans[top][_] = num
-                num += 1
-            top += 1
-
-            for _ in range(top, down+1):
-                ans[_][right] = num
-                num += 1
-            right -= 1
-
-            for _ in range(right, left-1, -1):
-                ans[down][_] = num
-                num += 1
-            down -= 1
-
-            for _ in range(down, top-1, -1):
-                ans[_][left] = num
-                num += 1
-            left += 1
+        while low > 1:
+            low, high = low - len(ans), low
+            ans = [[i for i in range(low, high)]]\
+                + [list(j) for j in zip(*ans[::-1])]
 
         return ans
 
