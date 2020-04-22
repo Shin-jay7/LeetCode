fadbbe108d849e1850b0c65e1f353b46e048b74b
Shin Ikeji <shin@jay7.net>
2020-04-22 11:52:35 -0700

    add sample outputs

--- 0059-Spiral_Matrix2.py
+++ 0059-Spiral_Matrix2.py
@@ -11,11 +11,32 @@ class Solution:
         ans, low = [], n*n+1
 
         while low > 1:
-            low, high = low - len(ans), low
+            low, high = low-len(ans), low
             ans = [[i for i in range(low, high)]]\
                 + [list(j) for j in zip(*ans[::-1])]
 
         return ans
+
+        """
+        low: 10
+        high: 10
+        [[]]
+        low: 9
+        high: 10
+        [[9]]
+        low: 8
+        high: 9
+        [[8], [9]]
+        low: 6
+        high: 8
+        [[6, 7], [9, 8]]
+        low: 4
+        high: 6
+        [[4, 5], [9, 6], [8, 7]]
+        low: 1
+        high: 4
+        [[1, 2, 3], [8, 9, 4], [7, 6, 5]]
+        """
 
 
 test = Solution()
