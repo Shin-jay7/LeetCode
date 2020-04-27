934448e162a879cdfd696064f11fed69d8003734
Shin Ikeji <shin@jay7.net>
2020-04-27 16:13:10 -0700

    update a new file

---
+++ 0065-Valid_Number.py
@@ -0,0 +1,89 @@
+from __future__ import annotations
+import re
+
+
+class Solution:
+    def isNumber(self, s: str) -> bool:
+        states = [{},
+                  # initial state
+                  {"blank": 1, "sign": 2, "digit": 3, ".": 4},
+                  # found sign
+                  {"digit": 3, ".": 4},
+                  # digit consumer (loop until non-digit)
+                  {"digit": 3, ".": 5, "e": 6, "blank": 9},
+                  # found dot (only a digit is valid)
+                  {"digit": 5},
+                  # after dot (expect digits, e or end of valid input)
+                  {"digit": 5, "e": 6, "blank": 9},
+                  # found "e" (only sign or digit valid)
+                  {"sign": 7, "digit": 8},
+                  # sign after "e" (only digit)
+                  {"digit": 8},
+                  # digit after "e" (expect digits or end of valid input)
+                  {"digit": 8, "blank": 9},
+                  # terminal state (fail if non-blank found)
+                  {"blank": 9}
+        ]
+
+        currentState = 1
+
+        for c in s:
+            if c in "0123456789":
+                c = "digit"
+            elif c in " \t\n":
+                c = "blank"
+            elif c in "+-":
+                c = "sign"
+
+            if c not in states[currentState]:
+                return False
+
+            currentState = states[currentState][c]
+
+        if currentState not in [3, 5, 8, 9]:
+            return False
+
+        return True
+
+
+test = Solution()
+test.isNumber("0") # True
+
+test = Solution()
+test.isNumber(" 0.1 ") # True
+
+test = Solution()
+test.isNumber("abc") # False
+
+test = Solution()
+test.isNumber("1 a") # False
+
+test = Solution()
+test.isNumber("2e10") # True
+
+test = Solution()
+test.isNumber(" -90e3   ") # True
+
+test = Solution()
+test.isNumber(" 1e") # False
+
+test = Solution()
+test.isNumber("e3") # False
+
+test = Solution()
+test.isNumber(" 6e-1") # True
+
+test = Solution()
+test.isNumber(" 99e2.5 ") # False
+
+test = Solution()
+test.isNumber("53.5e93") # True
+
+test = Solution()
+test.isNumber(" --6 ") # False
+
+test = Solution()
+test.isNumber("-+3") # False
+
+test = Solution()
+test.isNumber("95a54e53") # False
