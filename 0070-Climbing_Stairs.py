e96be063c6946682d2c7791fadd5abe23ec7ed9d
Shin Ikeji <shin@jay7.net>
2020-04-28 17:14:13 -0700

    A bit faster

--- 0070-Climbing_Stairs.py
+++ 0070-Climbing_Stairs.py
@@ -3,14 +3,11 @@ from __future__ import annotations
 
 class Solution:
     def climbStairs(self, n: int) -> int:
-        if n == 1: return 1
-        ans = [1,1]+[0]*(n-1)
+        a = b = 1
+        for _ in range(n):
+            a, b = b, a+b
 
-        for i in range(2,n+1):
-            ans[i] = ans[i-1]+ans[i-2]
-
-        return ans[-1]
-        # print(ans[-1])
+        return a
 
 test = Solution()
 test.climbStairs(1) # 1
