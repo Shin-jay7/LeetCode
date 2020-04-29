218dadff3533e60cb9376b615dfdaa8fe7bc85dd
Shin Ikeji <shin@jay7.net>
2020-04-29 11:27:11 -0700

    Shorter version with lru_cache

--- 0072-Edit_Distance.py
+++ 0072-Edit_Distance.py
@@ -1,44 +1,26 @@
 from __future__ import annotations
+from functools import lru_cache
 
 
+# https://stackoverflow.com/questions/49883177/how-does-lru-cache-from-functools-work
 class Solution:
     def minDistance(self, word1: str, word2: str) -> int:
-        m, n = len(word1), len(word2)
-        # Why does this cause the wrong answer?
-        # dp = [[0]*(n+1)]*(m+1)
-        dp = [[0] * (n + 1) for _ in range(m + 1)]
+        @lru_cache(None)
 
-        """
-        Define the state dp[i][j] is edit distance from
-        word1's first i letters to word2's first j letters.
-        To convert a string to an empty string, the mininum
-        number of operations (deletions) is just the length of
-        the string. So we have dp[i][0] = i and dp[0][j] = j.
-        """
-        for i in range(m+1):
-            dp[i][0] = i
+        def dp(i,j):
+            if i < 0 or j < 0:
+                return max(i,j)+1
+            if word1[i] == word2[j]:
+                return dp(i-1, j-1)
+            else:
+                return min(dp(i-1,j), dp(i-1,j-1), dp(i,j-1)) + 1
 
-        for j in range(n+1):
-            dp[0][j] = j
-
-        for i in range(1,m+1):
-            for j in range(1,n+1):
-                if word1[i-1] == word2[j-1]:
-                    dp[i][j] = dp[i-1][j-1]
-                else:
-                    # dp[i-1][j-1] + 1 (replace)
-                    # dp[i-1][j] + 1 (remove)
-                    # dp[i][j-1] + 1 (insert)
-                    dp[i][j] = 1 + min(dp[i-1][j], dp[i][j-1], dp[i-1][j-1])
-
-        return dp[-1][-1]
-        # print(dp[-1][-1])
-        # print(dp)
+        return dp(len(word1)-1, len(word2)-1)
+        # print(dp(len(word1)-1, len(word2)-1))
 
 
-
-# test = Solution()
-# test.minDistance("horse", "ros") # 3
+test = Solution()
+test.minDistance("horse", "ros") # 3
 
 test = Solution()
 test.minDistance("intention", "execution") # 5
