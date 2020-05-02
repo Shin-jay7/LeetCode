637d43fe9128729bd686dad68e1012a6c90a3449
Shin Ikeji <shin@jay7.net>
2020-05-02 09:28:16 -0700

    Non-stack approach: slow but intuitive

--- 0084-Largest_Rectangles_in_Histogram.py
+++ 0084-Largest_Rectangles_in_Histogram.py
@@ -3,27 +3,39 @@ from __future__ import annotations
 
 class Solution:
     def largestRectangleArea(self, heights: List[int]) -> int:
-        length = len(heights)
-        l, r, max_ = 0, 0, 0
+        n = len(heights)
+        left, right = [1]*n, [1]*n
+        """
+        left[i] to indicate how many continuous bars to the left (including
+        the bar at index i) are equal or higher than bar[i], right[i] is that
+        to the right of bar[i], so the the square of the max rectangle
+        containing bar[i] is simply height[i] * (left[i] + right[i] - 1)
+        """
 
-        while l < length:
-            if heights[l]:
-                H = heights[l]
-                while r < length:
-                    if l > r:
-                        r = l
-                        continue
-                    if not heights[r]:
-                        break
-                    H = min(H, heights[r])
-                    W = (r-l) + 1
-                    max_ = max(max_, H*W)
-                    r += 1
-            l += 1
-            r = 0
+        # calculate left
+        for i in range(1,n):
+            j = i-1
+            """
+            The while loop in the for loop jumps forward or backward
+            by the steps already calculated.
+            """
+            while j >= 0 and heights[j] >= heights[i]:
+                j -= left[j]
+            left[i] = i-j
 
-        return max_
-        # print(max_)
+        # calculate right
+        for i in range(n-2,-1,-1):
+            j = i+1
+            while j < n and heights[i] <= heights[j]:
+                j += right[j]
+            right[i] = j-i
+
+        ans = 0
+        for i in range(n):
+            ans = max(ans, heights[i]*(left[i]+right[i]-1))
+
+        return ans
+        # print(ans)
 
 
 test = Solution()
