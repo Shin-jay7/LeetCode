25edca23a0eb16c20247e400a5d637793f827817
Shin Ikeji <shin@jay7.net>
2020-05-05 20:31:37 -0700

    Add print-out function for test

--- 0095-Unique_Binary_Search_Trees_2.py
+++ 0095-Unique_Binary_Search_Trees_2.py
@@ -16,6 +16,51 @@ class TreeNode:
         self.left = left
         self.right = right
 
+    def _display_aux(self):
+        """Returns list of strings, width, height, and horizontal coordinate of the root."""
+        # No child.
+        if self.right is None and self.left is None:
+            line = '%s' % self.val
+            width = len(line)
+            height = 1
+            middle = width // 2
+            return [line], width, height, middle
+
+        # Only left child.
+        if self.right is None:
+            lines, n, p, x = self.left._display_aux()
+            s = '%s' % self.val
+            u = len(s)
+            first_line = (x + 1) * ' ' + (n - x - 1) * '_' + s
+            second_line = x * ' ' + '/' + (n - x - 1 + u) * ' '
+            shifted_lines = [line + u * ' ' for line in lines]
+            return [first_line, second_line] + shifted_lines, n + u, p + 2, n + u // 2
+
+        # Only right child.
+        if self.left is None:
+            lines, n, p, x = self.right._display_aux()
+            s = '%s' % self.val
+            u = len(s)
+            first_line = s + x * '_' + (n - x) * ' '
+            second_line = (u + x) * ' ' + '\\' + (n - x - 1) * ' '
+            shifted_lines = [u * ' ' + line for line in lines]
+            return [first_line, second_line] + shifted_lines, n + u, p + 2, u // 2
+
+        # Two children.
+        left, n, p, x = self.left._display_aux()
+        right, m, q, y = self.right._display_aux()
+        s = '%s' % self.val
+        u = len(s)
+        first_line = (x + 1) * ' ' + (n - x - 1) * '_' + s + y * '_' + (m - y) * ' '
+        second_line = x * ' ' + '/' + (n - x - 1 + u + y) * ' ' + '\\' + (m - y - 1) * ' '
+        if p < q:
+            left += [n * ' '] * (q - p)
+        elif q < p:
+            right += [m * ' '] * (p - q)
+        zipped_lines = zip(left, right)
+        lines = [first_line, second_line] + [a + u * ' ' + b for a, b in zipped_lines]
+        return lines, n + m + u, max(p, q) + 2, n + u // 2
+
 
 # list comprehension approach
 class Solution:
@@ -39,29 +84,47 @@ class Solution:
         return trees(1,n)
 
 
-# loop approach
-class Solution:
-    def generateTrees(self, n: int) -> List[TreeNode]:
-        if n == 0: return []
+# # loop approach
+# class Solution:
+#     def generateTrees(self, n: int) -> List[TreeNode]:
+#         if n == 0: return []
 
-        return self.generate(1, n)
+#         return self.generate(1, n)
 
-    @lru_cache(None)
-    def generate(self, first, last):
-        trees = []
-        for root in range(first, last+1):
-            for left in self.generate(first, root-1):
-                for right in self.generate(root+1, last):
-                    node = TreeNode(root)
-                    node.left = left
-                    node.right = right
-                    trees += node,
+#     @lru_cache(None)
+#     def generate(self, first, last):
+#         trees = []
+#         for root in range(first, last+1):
+#             for left in self.generate(first, root-1):
+#                 for right in self.generate(root+1, last):
+#                     node = TreeNode(root)
+#                     node.left = left
+#                     node.right = right
+#                     trees += node,
 
-        return trees or [None]
+#         return trees or [None]
+
+
+class BstNode:
+    def __init__(self, node):
+        self.node = node
+        self.val = node.val
+        self.right = node.right
+        self.left = node.left
+
+    def display(self):
+        lines, _, _, _ = self.node._display_aux()
+        for line in lines:
+            print(line)
 
 
 test = Solution()
 ans = test.generateTrees(3)
+for node in ans:
+    print("------------------------------")
+    b = BstNode(node)
+    b.display()
+    print("------------------------------")
 """
 [
   [1,None,3,2],
@@ -71,3 +134,4 @@ ans = test.generateTrees(3)
   [1,None,2,None,3]
 ]
 """
+
