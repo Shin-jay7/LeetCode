61153fe8fc2e81974ee0d6e6dce63bd7c1ad2471
Shin Ikeji <shin@jay7.net>
2020-05-14 09:52:56 -0700

    First attempt

---
+++ 0109-Convert_Sorted_List_to_Binary_Search_Tree.py
@@ -0,0 +1,120 @@
+from __future__ import annotations
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+    def _display_aux(self):
+        # Returns list of strings, width, height, and horizontal
+        # coordinate of the root.
+
+        # No child.
+        if self.right is None and self.left is None:
+            line = '%s' % self.val
+            width = len(line)
+            height = 1
+            middle = width // 2
+
+            return [line], width, height, middle
+
+        # Only left child
+        if self.right is None:
+            lines, n, p, x = self.left._display_aux()
+            s = '%s' % self.val
+            u = len(s)
+            first_line = (x+1) * ' ' + (n-x-1) * '_' + s
+            second_line = x * ' ' + '/' + (n-x-1+u) * ' '
+            shifted_lines = [line + u * ' ' for line in lines]
+
+            return [first_line, second_line] +\
+                    shifted_lines, n + u, p + 2, n + u // 2
+
+        # Only right child
+        if self.left is None:
+            lines, n, p, x = self.right._display_aux()
+            s = '%s' % self.val
+            u = len(s)
+            first_line = s + x * '_' + (n-x) * ' '
+            second_line = (u + x) * ' ' + '\\' + (n-x-1) * ' '
+            shifted_lines = [u * ' ' + line for line in lines]
+
+            return [first_line, second_line] +\
+                    shifted_lines, n + u, p + 2, u // 2
+
+        # Two children
+        left, n, p, x = self.left._display_aux()
+        right, m, q, y = self.right._display_aux()
+        s = '%s' % self.val
+        u = len(s)
+        first_line = (x+1) * ' ' + (n-x-1) * '_' + s + y * '_' + (m-y) * ' '
+        second_line = x * ' ' + '/' + (n-x-1+u+y) * ' ' + '\\' + (m-y-1) * ' '
+        if p < q:
+            left += [n * ' '] * (q-p)
+        elif q < p:
+            right += [m * ' '] * (p-q)
+        zipped_lines = zip(left, right)
+        lines = [first_line, second_line] +\
+                [a + u * ' ' + b for a,b in zipped_lines]
+
+        return lines, n + m + u, max(p, q) + 2, n + u // 2
+
+
+class BstNode:
+    def __init__(self, node):
+        self.node = node
+        self.val = node.val
+        self.right = node.right
+        self.left = node.left
+
+    def display(self):
+        lines, _, _, _ = self.node._display_aux()
+        for line in lines:
+            print(line)
+
+
+class Solution:
+    def sortedListToBST(self, head: ListNode) -> TreeNode:
+        if not head: return None
+
+        def convert(left, right):
+            if left > right: return None
+
+            mid = (left+right) // 2
+            node = TreeNode(nums[mid])
+            node.left = convert(left, mid-1)
+            node.right = convert(mid+1, right)
+
+            return node
+
+        cnt, nums = 0, []
+        while head:
+            nums.append(head.val)
+            head = head.next
+            cnt += 1
+
+        return convert(0, cnt-1)
+
+
+test = Solution()
+ans = test.sortedListToBST(ListNode(-10, ListNode(-3, ListNode(0,\
+                           ListNode(5, ListNode(9))))))
+print("------------------------------")
+b = BstNode(ans)
+b.display()
+print("------------------------------")
+"""
+     0_
+    /  \
+  _-3  9
+ /    /
+-10   5
+"""
