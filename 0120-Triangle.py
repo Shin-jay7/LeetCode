f81117cb6ef5ddb0b3086cbf84facdbd9ad9c065
Shin Ikeji <shin@jay7.net>
2020-05-23 12:29:41 -0700

    Bottom up approach results in shorter code

--- 0120-Triangle.py
+++ 0120-Triangle.py
@@ -3,22 +3,15 @@ from __future__ import annotations
 
 class Solution:
     def minimumTotal(self, triangle: List[List[int]]) -> int:
-        length = len(triangle)
-        dp = [[0]*(_+1) for _ in range(length)]
+        if not triangle: return
+        dp = triangle[-1]
 
-        for i in range(length):
+        for i in range(len(triangle)-2, -1, -1):
             for j in range(i+1):
-                if i > 1 and 0 < j < i:
-                    dp[i][j] =\
-                    min(dp[i-1][j-1], dp[i-1][j])
-                elif i > 0 and j == 0:
-                    dp[i][j] = dp[i-1][j]
-                elif i > 0 and j == i:
-                    dp[i][j] = dp[i-1][j-1]
-                dp[i][j] += triangle[i][j]
+                dp[j] = min(dp[j], dp[j+1]) + triangle[i][j]
 
-        return min(dp[-1])
-        # print(min(dp[-1]))
+        return dp[0]
+        # print(dp[0])
 
 
 test = Solution()
