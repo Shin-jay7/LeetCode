4cbe2f4029f3a05e54547cab3cc37dae34cc36ab
Shin Ikeji <shin@jay7.net>
2020-05-29 16:43:02 -0700

    Fix indent

--- 0126-World_Ladder_2.py
+++ 0126-World_Ladder_2.py
@@ -24,22 +24,21 @@ class Solution:
                                 matchedQ.add(word)
                             tree[word].add(bWord) if reverseKeyValue\
                                                   else tree[bWord].add(word)
-                beginQ, matchedQ = matchedQ, set()
+            beginQ, matchedQ = matchedQ, set()
 
-                """
-                BFS't time complexity is O(b^d) where b is branch factor
-                and d is depth. So if we go with a bi-directional way,
-                expanding from both being word and end word, and choosing
-                the queue ('begin' queue or 'end' queue) with smaller size
-                in each expansion, the branch factor will be greatly reduced.
-                """
-                if len(beginQ) > len(endQ):
-                    beginQ, endQ = endQ, beginQ
-                    reverseKeyValue = not reverseKeyValue
+            """
+            BFS't time complexity is O(b^d) where b is branch factor
+            and d is depth. So if we go with a bi-directional way,
+            expanding from both being word and end word, and choosing
+            the queue ('begin' queue or 'end' queue) with smaller size
+            in each expansion, the branch factor will be greatly reduced.
+            """
+            if len(beginQ) > len(endQ):
+                beginQ, endQ, reverseKeyValue =\
+                endQ, beginQ, not reverseKeyValue
 
         def bt(x):
-            return [[x]] if x == endWord\
-                   else [[x]+rest for y in tree[x] for rest in bt(y)]
+            return [[x]] if x == endWord else [[x] + rest for y in tree[x] for rest in bt(y)]
 
         return bt(beginWord)
         # print(bt(beginWord))
@@ -55,7 +54,15 @@ test.findLadders("hit", "cog", ["hot","dot","dog","lot","log","cog"])
 """
 
 test = Solution()
-test.findLadders("hit", "cog",  ["hot","dot","dog","lot","log"])
+test.findLadders("hit", "cog", ["hot","dot","dog","lot","log"])
 """
 []
+"""
+
+test = Solution()
+test.findLadders("kiss", "tusk",\
+ ["miss","dusk","kiss","musk","tusk","diss","disk","sang","ties","muss"])
+"""
+[["kiss","diss","disk","dusk","tusk"],\
+ ["kiss","miss","muss","musk","tusk"]]
 """
