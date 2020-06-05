4f6e9ee37ab95c27cb35a78da84d6401918e84ad
Shin Ikeji <shin@jay7.net>
2020-06-05 08:55:33 -0700

    Add new file

---
+++ 0137-Single_Number_2.py
@@ -0,0 +1,27 @@
+from __future__ import annotations
+
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        return (3*sum(set(nums)) - sum(nums))//2
+
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        nums.sort()
+
+        for i in range(2,len(nums),3):
+            if not (nums[i] == nums[i-1] and nums[i] == nums[i-2]):
+                return nums[i-2]
+                # print(nums[i-2])
+                # return
+
+        return nums[-1]
+        # print(nums[-1])
+
+
+test = Solution()
+test.singleNumber([2,2,3,2]) # 3
+
+test = Solution()
+test.singleNumber([0,1,0,1,0,1,99]) # 99
