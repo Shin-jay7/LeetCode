6c3347611107be4a037f3fe4d9466feb901f0289
Shin Ikeji <shin@jay7.net>
2020-06-11 11:27:16 -0700

    Two more approaches

--- 0140-Word_Break_2.py
+++ 0140-Word_Break_2.py
@@ -23,7 +23,48 @@ class Solution:
                 return
             for i in range(1, n+1):
                 if s[:i] in dic:
-                    self.dfs(s[i:], len(s[i:]), dp, idx+i, dic, path+s[:i]+" ", ans)
+                    self.dfs(s[i:], len(s[i:]), dp, idx+i,\
+                             dic, path+s[:i]+" ", ans)
+
+
+from functools import lru_cache
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> List[str]:
+        @lru_cache()
+
+        def dp(i):
+            ans = []
+            for word in wordDict:
+                if word != s[i:i+len(word)]:
+                    continue
+                elif len(word) == len(s)-i:
+                    ans.append(word)
+                else:
+                    for rest in dp(i+len(word)):
+                        ans.append(word + ' ' + rest)
+
+            return ans
+
+        return dp(0)
+        # print(dp(0))
+
+# Concise but slow
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> List[str]:
+        memo = {len(s): ['']}
+
+        def sentences(i):
+            if i not in memo:
+                memo[i] = [s[i:j] + (tail and ' ' + tail)
+                           for j in range(i+1, len(s)+1)
+                           if s [i:j] in wordDict
+                           for tail in sentences(j)]
+
+            return memo[i]
+
+        return sentences(0)
+        # print(sentences(0))
+
 
 
 test = Solution()
