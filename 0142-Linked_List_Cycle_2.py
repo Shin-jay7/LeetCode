097f50387312554a37881aa60a751f76dd1c6061
Shin Ikeji <shin@jay7.net>
2020-06-13 09:01:55 -0700

    First stub

---
+++ 0142-Linked_List_Cycle_2.py
@@ -0,0 +1,24 @@
+from __future__ import annotations
+
+
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution:
+    def detectCycle(self, head: ListNode) -> ListNode:
+        slow = fast = head
+
+        while fast and fast.next:
+            fast = fast.next.next
+            slow = slow.next
+            if slow == fast:
+                while head:
+                    if head == slow:
+                        return head
+                    head = head.next
+                    slow = slow.next
+
+        return None
