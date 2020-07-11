4b34280ffea4cacec46ed74919e164bc9e6ee12f
Shin Ikeji <shin@jay7.net>
2020-07-11 12:07:08 -0700

    Shorter version

--- 0165-Compare_Version_Numbers.py
+++ 0165-Compare_Version_Numbers.py
@@ -1,29 +1,12 @@
 from __future__ import annotations
 
-
+from itertools import zip_longest
 class Solution:
     def compareVersion(self, version1: str, version2: str) -> int:
-        v1 = list(map(int, version1.split(".")))
-        v2 = list(map(int, version2.split(".")))
-        l1, l2 = len(v1), len(v2)
-        if l1 > l2:
-            v2 += [0]*(l1-l2)
-        elif l1 < l2:
-            v1 += [0]*(l2-l1)
+        v1, v2 = map(int, version1.split(".")), map(int, version2.split("."))
+        v1, v2 = zip(*zip_longest(v1, v2, fillvalue=0))
 
-        for i in range(max(l1, l2)):
-            if v1[i] > v2[i]:
-                return 1
-                # print(1)
-                # return
-            elif v1[i] < v2[i]:
-                return -1
-                # print(-1)
-                # return
-
-        return 0
-        # print(0)
-        # return
+        return (0,1,-1)[(v1>v2)-(v1<v2)]
 
 
 test = Solution()
@@ -40,6 +23,3 @@ test.compareVersion("1.01", "1.001") # 0
 
 test = Solution()
 test.compareVersion("1.0", "1.0.0") # 0
-
-# test = Solution()
-# test.compareVersion()
