ecc0fd4764f69a12c8a6bc5b077a40ba0d202d37
Shin Ikeji <shin@jay7.net>
2021-01-09 14:31:29 -0800

    Two approaches

--- 0198-House_Robber.py
+++ 0198-House_Robber.py
@@ -6,14 +6,32 @@ f(1) = max(num[0], num[1])
 f(k) = max(f(k-2) + nums[k], f(k-1))
 """
 
+# class Solution:
+#     def rob(self, nums: List[int]) -> int:
+#         prev = curr = 0
+#         for num in nums:
+#             prev, curr = curr, max(prev + num, curr)
+
+#         return curr
+#         # print(curr)
+
+
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        prev = curr = 0
-        for num in nums:
-            prev, curr = curr, max(prev + num, curr)
+        length = len(nums)
+        memo = [-1] * (length+1)
 
-        return curr
-        # print(curr)
+        return self._rob(nums, length-1, memo)
+
+    def _rob(self, nums, i, memo):
+        if i < 0:
+            return 0
+        if memo[i] >= 0:
+            return memo[i]
+        result = max(_rob(nums, i-2, memo)+nums[i], _rob(nums, i-1, memo))
+        memo[i] = result
+        return result
+
 
 
 test = Solution()
