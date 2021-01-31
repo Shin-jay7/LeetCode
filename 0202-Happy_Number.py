bd066dc44605b5a2e5d3294db37471a5a7ca6f19
Shin Ikeji <shin@jay7.net>
2021-01-31 12:01:30 -0800

    First attempt

---
+++ 0202-Happy_Number.py
@@ -0,0 +1,11 @@
+from __future__ import annotations
+
+
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        seen = set()
+        while n not in seen:
+            seen.add(n)
+            n = sum(int(i)**2 for i in str(n))
+
+        return n == 1
