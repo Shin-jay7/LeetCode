34477054741fdaff564704a3b4cff36eee50ee08
Shin Ikeji <shin@jay7.net>
2021-02-11 17:06:55 -0800

    Two solutions

---
+++ 0206-Reverse_Linked_List.py
@@ -0,0 +1,30 @@
+from __future__ import annotations
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def reverseList(self, head: ListNode) -> ListNode:
+        prev = None
+        while head:
+            curr = head
+            head = head.next
+            curr.next = prev
+            prev = curr
+        return prev
+
+
+class Solution:
+    def reverseList(self, head: ListNode) -> ListNode:
+        return self._reverse(head)
+
+    def _reverse(self, node, prev=None):
+        if not node:
+            return prev
+        n = node.next
+        node.next = prev
+        return self._reverse(n, node)
