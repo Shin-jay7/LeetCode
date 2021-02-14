d1f98c8ca92b218b6ca02ed3262322d2d3cf8c91
Shin Ikeji <shin@jay7.net>
2021-02-14 07:24:17 -0800

    Two solutions

---
+++ 0208-Implement_Trie.py
@@ -0,0 +1,53 @@
+from __future__ import annotations
+
+
+class Trie:
+    def __init__(self):
+        self.trie = {}
+
+    def insert(self, word: str) -> None:
+        t = self.trie
+        for w in word:
+            if w not in t:
+                t[w] = {}
+            t = t[w]
+        t['#'] = '#'
+
+    def search(self, word: str) -> bool:
+        t = self.trie
+        for w in word:
+            if w not in t:
+                return False
+            t = t[w]
+        if '#' in t:
+            return True
+        return False
+
+    def startsWith(self, prefix: str) -> bool:
+        t = self.trie
+        for p in prefix:
+            if p not in t:
+                return False
+            t = t[p]
+        return True
+
+
+class Trie:
+    def __init__(self):
+        self.setWord = set()
+        self.setPrefix = set()
+
+    def insert(self, word: str) -> None:
+        self.setWord.add(word)
+        for i in range(len(word)):
+            self.setPrefix.add(word[:i+1])
+
+    def search(self, word: str) -> bool:
+        if word in self.setWord:
+            return True
+        return False
+
+    def startsWith(self, prefix: str) -> bool:
+        if prefix in self.setPrefix:
+            return True
+        return False
