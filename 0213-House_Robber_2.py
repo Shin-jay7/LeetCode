5a17c48db47cf838fea17ec5429a11845acf21f2
Shin Ikeji <shin@jay7.net>
2021-02-19 09:00:22 -0800

    Upload a new file

---
+++ 0213-House_Robber_2.py
@@ -0,0 +1,11 @@
+from __future__ import annotations
+
+
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        def rob(nums):
+            now = prev = 0
+            for n in nums:
+                now, prev = max(now, prev + n), now
+            return now
+        return max(rob(nums[len(nums) != 1:]), rob(nums[:-1]))
