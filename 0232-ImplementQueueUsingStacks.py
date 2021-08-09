04d38dd4ea88687cb0376077679f6b9ec2c207ee
Shin Ikeji <shin@jay7.net>
2021-08-09 13:18:07 +0900

    Use collections.dequeu

---
+++ 0232-ImplementQueueUsingStacks.py
@@ -0,0 +1,19 @@
+from __future__ import annotations
+from collections import deque
+
+
+class MyStack:
+    def __init__(self):
+        self.queue = deque()
+
+    def push(self, x: int) -> None:
+        self.queue.append(x)
+
+    def pop(self) -> int:
+        return self.queue.popleft()
+        
+    def peek(self) -> int:
+        return self.queue[0]
+
+    def empty(self) -> bool:
+        return len(self.queue) == 0
