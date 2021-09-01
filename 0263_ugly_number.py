eb0764de791adb33d6e8d6c717907e2c1b2c9469
Shin Ikeji <shin@jay7.net>
2021-09-01 06:51:34 -0700

    Time exceeded

---
+++ 0263_ugly_number.py
@@ -0,0 +1,18 @@
+from __future__ import annotations
+
+
+class Solution:
+    def isUgly(self, n: int) -> bool:
+        while n % 2 == 0:
+            n //= 2
+        while n % 3 == 0:
+            n //= 3
+        while n % 5 == 0:
+            n //= 5
+            
+        return n == 1
+        # print(n == 1)
+
+
+test = Solution()
+test.isUgly(6) # True
