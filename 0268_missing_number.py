b9b9a610f557fc818c58acef7594a01eb778f927
Shin Ikeji <shin@jay7.net>
2021-09-01 20:15:05 -0700

    Accepted but very slow

---
+++ 0268_missing_number.py
@@ -0,0 +1,9 @@
+from __future__ import annotations
+
+
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        for i in range(len(nums)+1):
+            if i not in nums:
+                return i
+                
