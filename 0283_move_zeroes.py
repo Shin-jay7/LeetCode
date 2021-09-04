0a9dc243e633b672be4c38f3cc781bb5162b3b51
Shin Ikeji <shin@jay7.net>
2021-09-03 20:27:11 -0700

    Brilliant way

--- 0283_move_zeroes.py
+++ 0283_move_zeroes.py
@@ -4,10 +4,12 @@ from typing import List
 
 class Solution:
     def moveZeroes(self, nums: List[int]) -> None:
-        for _ in range(nums.count(0)):
-            nums.remove(0)
-            nums.append(0)
-            
+        i = 0
+        for j in range(len(nums)):
+            if nums[j] != 0:
+                nums[i], nums[j] = nums[j], nums[i]
+                i += 1
+
 
 test = Solution()
 test.moveZeroes([0,1,0,3,12]) # [1,3,12,0,0]
