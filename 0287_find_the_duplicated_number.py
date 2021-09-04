3072661b8a11ac1739084dbf59dea4990a9a9211
Shin Ikeji <shin@jay7.net>
2021-09-04 11:08:02 -0700

    Find the beggining of the loop in a linked list

--- 0287_find_the_duplicated_number.py
+++ 0287_find_the_duplicated_number.py
@@ -2,25 +2,35 @@ from __future__ import annotations
 from typing import List
 
 
+"""
+the problem is to find the starting point of loop in singly-linked list,
+which has a classical solution with two pointers: 
+slow which moves one step at a time and fast, which moves two times at a time.
+To find this place we need to do it in two iterations: first we wait until
+fast pointer gains slow pointer and then we move slow pointer to the start
+and run them with the same speed and wait until they concide.
+"""
 class Solution:
     def findDuplicate(self, nums: List[int]) -> int:
-        low, high = 0, len(nums) - 1
-        mid = (low + high) // 2
-        # print(low, mid, high)
+        slow, fast = nums[0], nums[0]
+        # print("Begin")
+        # print(slow, fast)
+        # print("First loop")
+        while True:
+            slow, fast = nums[slow], nums[nums[fast]]
+            # print(slow, fast)
+            if slow == fast:
+                break
 
-        while high > low:
-            cnt = 0
-            for n in nums:
-                if mid < n <= high:
-                    cnt += 1
-            if cnt > high - mid: # then we have duplicates between mid-high
-                low = mid + 1
-            else:
-                high = mid
-            mid = (low + high) // 2
-            # print(low, mid, high)
+        slow = nums[0]
+        print("Second loop")
+        while slow != fast:
+            slow, fast = nums[slow], nums[fast]
+            # print(slow, fast)
 
-        return high
+        return slow
+
+# https://leetcode.com/problems/find-the-duplicate-number/discuss/704693/Python-2-solutions%3A-Linked-List-Cycle-O(n)-and-BS-O(n-log-n)-explained
 
 
 test = Solution()
