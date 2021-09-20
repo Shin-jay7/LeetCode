b8e9a034489062bce0892979a01158c5bc7e45ea
Shin Ikeji <shin@jay7.net>
2021-09-19 21:28:47 -0700

    Fix map requires two or more arguments

--- 0299_bulls_and_cows.py
+++ 0299_bulls_and_cows.py
@@ -4,7 +4,7 @@ import operator
 
 class Solution:
     def getHint(self, secret: str, guess: str) -> str:
-        bulls = sum(map(operator.eq(secret, guess)))
+        bulls = sum(map(operator.eq, secret, guess))
         both = sum(min(secret.count(x), guess.count(x)) for x in set(guess))
         return str(bulls) + "A" + str(both-bulls) + "B"
 
