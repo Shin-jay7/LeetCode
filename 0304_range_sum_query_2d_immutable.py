d43b41ea60e7aacdbef700c923958bd44c313b8a
Shin Ikeji <shin@jay7.net>
2021-09-26 11:21:02 -0700

    DP solution

--- 0304_range_sum_query_2d_immutable.py
+++ 0304_range_sum_query_2d_immutable.py
@@ -4,10 +4,14 @@ from typing import List
 
 class NumMatrix:
     def __init__(self, matrix: List[List[int]]) -> None:
-        self.matrix = matrix
+        if not matrix:
+            return
+        m, n = len(matrix), len(matrix[0])
+        dp = self.dp = [[0] * (n+1) for _ in range(m+1)]
+        for i in range(m):
+            for j in range(n):
+                dp[i+1][j+1] = dp[i][j+1] + dp[i+1][j] + matrix[i][j] - dp[i][j]
 
     def sumRegion(self, row1: int, col1: int, row2: int, col2: int) -> int:
-        ans = 0
-        for i in range(row1, row2+1):
-            ans += sum(self.matrix[i][col1:col2+1])
-        return ans
+        dp = self.dp
+        return dp[row2+1][col2+1] - dp[row2+1][col1] - dp[row1][col2+1] + dp[row1][col1]
