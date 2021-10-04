c36a2862fc997e4bf19caca479aafb84fb0b6e7c
Shin Ikeji <shin@jay7.net>
2021-10-04 13:08:47 -0700

    This is a math problem

--- 0319_bulb_switcher.py
+++ 0319_bulb_switcher.py
@@ -1,26 +1,7 @@
 from __future__ import annotations
+from math import sqrt
 
 
 class Solution:
     def bulbSwitch(self, n: int) -> int:
-        if n == 0:
-            return 0
-        if n == 1:
-            return 1
-
-        bulbs = [1]*n
-        # print(bulbs)
-        for round in range(2, n):
-            for i in range(n):
-                if i % round == round-1:
-                    bulbs[i] ^= 1
-            # print(bulbs)
-
-        bulbs[-1] ^= 1
-        # print(bulbs)
-
-        return sum(bulbs)
-
-
-test = Solution()
-test.bulbSwitch(4) # 2
+        return int(sqrt(n))
