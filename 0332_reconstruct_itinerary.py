5db54e95bc568a68062889114023773fa15dda6a
Shin Ikeji <shin@jay7.net>
2021-10-12 15:40:01 -0700

    Eulerian path and circuit

---
+++ 0332_reconstruct_itinerary.py
@@ -0,0 +1,22 @@
+from __future__ import annotations
+from typing import List
+from collections import defaultdict
+
+
+class Solution:
+    def dfs(self, airport: str) -> None:
+        while self.adj_list[airport]:
+            candidate = self.adj_list[airport].pop()
+            self.dfs(candidate)
+        self.route.append(airport)
+
+    def findItinerary(self, tickets: List[List[str]]) -> List[str]:
+        self.route = []
+        self.adj_list = defaultdict(list)
+        for i, j in tickets:
+            self.adj_list[i].append(j)
+        for key in self.adj_list:
+            self.adj_list[key] = sorted(self.adj_list[key], reverse=True)
+        self.dfs("JFK")
+
+        return self.route[::-1]
