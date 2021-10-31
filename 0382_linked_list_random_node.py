bca6215e57f53daf5a59d2cedf2c8b61b45fb558
Shin Ikeji <shin@jay7.net>
2021-10-31 12:17:25 +0900

    Add reservoir sampling

--- 0382_linked_list_random_node.py
+++ 0382_linked_list_random_node.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 from typing import Optional
-from random import choice
+from random import choice, random
 
 
 class ListNode:
@@ -18,3 +18,23 @@ class Solution:
 
     def getRandom(self) -> int:
         return choice(self.vals)
+
+
+# https://florian.github.io/reservoir-sampling/
+class Solution:
+    def __init__(self, head: Optional[ListNode]):
+        self.head = head
+
+    def getRandom(self) -> int:
+        # nodes is number of nodes we count in list so far
+        # and cur is current node we are in.
+        nodes, cur = 1, 1
+        head, ans = self.head, self.head
+        while head.next:
+            nodes += 1
+            head = head.next
+            if random() < cur/nodes:
+                ans = ans.next
+                cur += 1
+
+        return ans.val
