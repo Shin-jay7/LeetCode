8ea94e071a62348236463a3cce3bc553491737a3
Shin Ikeji <shin@jay7.net>
2021-12-12 17:43:34 +0900

    Simple version

--- 0398_random_pick_index.py
+++ 0398_random_pick_index.py
@@ -1,15 +1,11 @@
 from __future__ import annotations
 from typing import List
-from collections import defaultdict
 from random import choice
 
 
 class Solution:
     def __init__(self, nums: List[int]):
-        d = defaultdict(list)
-        for idx, num in enumerate(nums):
-            d[num].append(idx)
-        self.nums = d
+        self.nums = nums
 
     def pick(self, target: int) -> int:
-        return choice(self.nums[target])
+        return choice([idx for idx, val in enumerate(self.nums) if val == target])
