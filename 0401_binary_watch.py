c943c49c64fdd1d55b8062df6620393ffbab675c
Shin Ikeji <shin@jay7.net>
2021-12-18 10:21:52 +0900

    turnedOn equals number of 1s in binary total

--- 0401_binary_watch.py
+++ 0401_binary_watch.py
@@ -5,6 +5,15 @@ from itertools import combinations
 
 class Solution:
     def readBinaryWatch(self, turnedOn: int) -> List[str]:
+        return [
+            str(h) + ":" + str(m).zfill(2)
+            for h in range(12) for m in range(60)
+            if (bin(h) + bin(m)).count("1") == turnedOn
+        ]
+
+
+class Solution:
+    def readBinaryWatch(self, turnedOn: int) -> List[str]:
         LED = [8, 4, 2, 1, 32, 16, 8, 4, 2, 1]
         hour, minute, ans = 0, 0, []
         for perm in combinations(range(10), turnedOn):
