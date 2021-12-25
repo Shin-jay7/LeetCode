a484cba39cf9d92b4f37441b5808559b471f59d1
Shin Ikeji <shin@jay7.net>
2021-12-26 07:58:56 +0900

    Shorter version

--- 0412_fizz_buzz.py
+++ 0412_fizz_buzz.py
@@ -4,19 +4,7 @@ from typing import List
 
 class Solution:
     def fizzBuzz(self, n: int) -> List[str]:
-        ans = []
-        for i in range(1, n+1):
-            if i % 3 == 0 and i % 5 == 0:
-                ans.append("FizzBuzz")
-            elif i % 3 == 0:
-                ans.append("Fizz")
-            elif i % 5 == 0:
-                ans.append("Buzz")
-            else:
-                ans.append(str(i))
-
-        print(ans)
-        return ans
+        return ["Fizz" * (i % 3 == 0) + "Buzz" * (i % 5 == 0) or str(i) for i in range(1, n+1)]
 
 
 test = Solution()
