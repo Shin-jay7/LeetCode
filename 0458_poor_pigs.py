4b917ff8a0e5be8770538cf1473b6909b3d99acd
Shin Ikeji <shin@jay7.net>
2022-04-21 12:46:59 +0900

    Calc required dimensions to find the num of pigs

---
+++ 0458_poor_pigs.py
@@ -0,0 +1,12 @@
+from __future__ import annotations
+
+
+# https://leetcode.com/problems/poor-pigs/discuss/94266/Another-explanation-and-solution
+class Solution:
+    def poorPigs(
+          self, buckets: int, minutesToDie: int, minutesToTest: int
+         ) -> int:
+        pigs = 0
+        while (minutesToTest // minutesToDie + 1) ** pigs < buckets:
+            pigs += 1
+        return pigs
