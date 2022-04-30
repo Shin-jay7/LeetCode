d3cfc645b64272d058a342bee3109b33db768b62
Shin Ikeji <shin@jay7.net>
2022-05-01 08:14:55 +0900

    Two solutions

--- 0477_total_hamming_distance.py
+++ 0477_total_hamming_distance.py
@@ -13,3 +13,10 @@ class Solution:
             ans += ones * zeros
 
         return ans
+
+
+class Solution:
+    def totalHammingDistance(self, nums: List[int]) -> int:
+        return sum(
+             b.count('0') * b.count('1') for b in zip(*map('{:032b}'.format, nums))
+            )
