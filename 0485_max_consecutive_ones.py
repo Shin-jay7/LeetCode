3f3f7b15c0bd980381d0e7014937e962a5ad4aa1
Shin Ikeji <shin@jay7.net>
2022-05-06 08:49:03 +0900

    from intertools import groupby

--- 0485_max_consecutive_ones.py
+++ 0485_max_consecutive_ones.py
@@ -1,16 +1,8 @@
 from __future__ import annotations
 from typing import List
+from itertools import groupby
 
 
 class Solution:
     def findMaxConsecutiveOnes(self, nums: List[int]) -> int:
-        cnt, _max = 0, 0
-        for n in nums:
-            if n == 1:
-                cnt += 1
-            else:
-                _max = max(_max, cnt)
-                cnt = 0
-            _max = max(_max, cnt)
-
-        return _max
+        return max(sum(g) for _, g in groupby(nums))
