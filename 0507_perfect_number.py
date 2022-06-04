4a713b6abc07d3ddf598e1f74977b89364ac5695
Shin Ikeji <shin@jay7.net>
2022-06-04 14:54:07 +0900

    Bit shorter and cleaner

--- 0507_perfect_number.py
+++ 0507_perfect_number.py
@@ -6,15 +6,12 @@ class Solution:
     def checkPerfectNumber(self, num: int) -> bool:
         if num == 1:
             return False
-        divisors = set([1])
+        ans = 1
         for n in range(2, int(math.sqrt(num)) + 1):
             if num % n == 0:
-                divisors.add(n)
-                divisors.add(num//n)
+                ans += n + num//n
 
-        # print(divisors)
-        # print(num == sum(divisors))
-        return num == sum(divisors)
+        return ans == num
 
 
 test = Solution()
