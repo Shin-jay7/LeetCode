bf5d3fc1e55cc031db1d998ca36dac5cdc5f449f
Shin Ikeji <shin@jay7.net>
2022-07-03 13:21:44 +0900

    Track rolling remainder

--- 0523_continuous_subarray_sum.py
+++ 0523_continuous_subarray_sum.py
@@ -2,15 +2,15 @@ from __future__ import annotations
 from typing import List
 
 
+# If sum(nums[i:j]) % == 0 when i < j,
+# then sum(nums[:j]) % k == sum(nums[:i]) % k
 class Solution:
     def checkSubarraySum(self, nums: List[int], k: int) -> bool:
-        length = len(nums)
-        if length < 2:
-            return False
-
-        for i in range(length):
-            for j in range(i+1, length):
-                if sum(nums[i:j+1]) % k == 0:
-                    return True
-
+        remainder, seen = 0, {0: -1}
+        for idx, num in enumerate(nums):
+            remainder = (remainder + num) % k
+            if remainder not in seen:
+                seen[remainder] = idx
+            elif idx - seen[remainder] > 1:
+                return True
         return False
