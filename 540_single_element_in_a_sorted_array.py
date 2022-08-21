8adafe1568327fa630bb49ef8bd7f977f40c78ab
Shin Ikeji <shin@jay7.net>
2022-08-21 20:50:12 +0900

    Use xor

---
+++ 540_single_element_in_a_sorted_array.py
@@ -0,0 +1,14 @@
+from __future__ import annotations
+from typing import List
+
+
+class Solution:
+    def singleNonDuplicate(self, nums: List[int]) -> int:
+        lo, hi = 0, len(nums) - 1
+        while lo < hi:
+            mid = (lo + hi) // 2
+            if nums[mid] == nums[mid ^ 1]:
+                lo = mid + 1
+            else:
+                hi = mid
+        return nums[lo]
