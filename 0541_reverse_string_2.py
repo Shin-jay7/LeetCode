9a6683ad5bbbb16e65d7f45e6e637fa0823ff764
Shin Ikeji <shin@jay7.net>
2022-08-27 11:08:38 +0900

    For every block of 2k characters starting with position i, replaces S[i:i+k] with it's reverse.

--- 0541_reverse_string_2.py
+++ 0541_reverse_string_2.py
@@ -3,19 +3,10 @@ from __future__ import annotations
 
 class Solution:
     def reverseStr(self, s: str, k: int) -> str:
-        ans = ""
-        while s:
-            size = len(s)
-            if size >= 2*k:
-                ans += s[:k][::-1] + s[k:2*k]
-            elif size >= k:
-                ans += s[:k][::-1] + s[k:]
-            else:
-                ans += s[::-1]
-            s = s[2*k:]
-
-        # print(ans)
-        return ans
+        s = list(s)
+        for i in range(0, len(s), 2*k):
+            s[i:i+k] = s[i:i+k][::-1]
+        return "".join(s)
 
 
 test = Solution()
