3d62f8899e91ae185c0d3ca4dcaf1429ed10f778
Shin Ikeji <shin@jay7.net>
2022-08-27 10:48:42 +0900

    First stub

---
+++ 0541_reverse_string_2.py
@@ -0,0 +1,27 @@
+from __future__ import annotations
+
+
+class Solution:
+    def reverseStr(self, s: str, k: int) -> str:
+        ans = ""
+        while s:
+            size = len(s)
+            if size >= 2*k:
+                ans += s[:k][::-1] + s[k:2*k]
+                s = s[2*k:]
+            elif size >= k:
+                ans += s[:k][::-1] + s[k:]
+                s = ""
+            else:
+                ans += s[::-1]
+                s = ""
+
+        # print(ans)
+        return ans
+
+
+test = Solution()
+test.reverseStr("abcdefg", 2) # "bacdfeg"
+
+test = Solution()
+test.reverseStr("abcd", 2) # "bacd"
