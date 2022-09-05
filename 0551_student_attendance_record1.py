ffa2520c84bf3f0ee98b7c10f93c5f3e9ccab96c
Shin Ikeji <shin@jay7.net>
2022-09-05 09:10:57 +0900

    One liner

--- 0551_student_attendance_record1.py
+++ 0551_student_attendance_record1.py
@@ -3,17 +3,4 @@ from __future__ import annotations
 
 class Solution:
     def checkRecord(self, s: str) -> bool:
-        cnt_a, cnt_l = 0, 0
-        for cha in s:
-            if cha == "L":
-                cnt_l += 1
-                if cnt_l > 2:
-                    return False
-            else:
-                cnt_l = 0
-            if cha == "A":
-                cnt_a += 1
-                if cnt_a > 1:
-                    return False
-
-        return True
+        return s.count("A") < 2 and "LLL" not in s
