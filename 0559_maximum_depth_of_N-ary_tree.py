e7493ca7dd63f5227041dd16abfb72b2836ed639
Shin Ikeji <shin@jay7.net>
2022-09-24 05:21:42 -0700

    Recursive approach to compare with each depth

---
+++ 0559_maximum_depth_of_N-ary_tree.py
@@ -0,0 +1,17 @@
+from __future__ import annotations
+
+
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+
+
+class Solution:
+    def maxDepth(self, root: 'Node') -> int:
+        if not root:
+            return 0
+        depth = 0
+        for child in root.children:
+            depth = max(depth, self.maxDepth(child))
+        return depth + 1
