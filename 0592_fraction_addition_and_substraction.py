6686d11c027ab20ce964f35c28dedd35cb62bc08
Shin Ikeji <shin@jay7.net>
2022-12-23 18:23:06 -0800

    Fix typo

--- 0592_fraction_addition_and_substraction.py
+++ 0592_fraction_addition_and_substraction.py
@@ -4,31 +4,33 @@ import re
 import math
 
 
-class Solution:
-    def fractionAddition(self, expression: str) -> str:
-        ans = sum(map(Fraction,
-                      expression.replace('+', ' +').replace('-', ' -').split())
-                  )
-        return '{0}/{1}'.format(ans.numerator, ans.denominator)
+# class Solution:
+#     def fractionAddition(self, expression: str) -> str:
+#         ans = sum(map(Fraction,
+#                       expression.replace('+', ' +').replace('-', ' -').split())
+#                   )
+#         return '{0}/{1}'.format(ans.numerator, ans.denominator)
 
 
-class Solution:
-    def fractionAddition(self, expression: str) -> str:
-        ans = sum(map(Fraction, re.findall('[+-]?\d+/\d+', expression)))
-        return '{0}/{1}'.format(ans.numerator, ans.denominator)
+# class Solution:
+#     def fractionAddition(self, expression: str) -> str:
+#         ans = sum(map(Fraction, re.findall('[+-]?\d+/\d+', expression)))
+#         return '{0}/{1}'.format(ans.numerator, ans.denominator)
 
 
 class Solution:
     def fractionAddition(self, expression: str) -> str:
         nums = map(int, re.findall('[+-]?\d+', expression))
         numerator, denominator = 0, 1
+        # for num in nums:
+        #     print(num)
         for num in nums:
             num_next = next(nums)
             numerator = numerator * num_next + denominator * num
             denominator *= num_next
             g = math.gcd(numerator, denominator)
             numerator //= g
-            denominator //=g
+            denominator //= g
         return '{0}/{1}'.format(numerator, denominator)
 
 
