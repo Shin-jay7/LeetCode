7d8938403a2eef0cee6c62832eb8b872cfe67d76
Shin Ikeji <shin@jay7.net>
2023-01-05 19:47:32 -0800

    Time Limit exceeded

---
+++ 0611_valid_triangle_number.py
@@ -0,0 +1,19 @@
+from __future__ import annotations
+from typing import List
+from itertools import combinations
+
+
+class Solution:
+    def triangleNumber(self, nums: List[int]) -> int:
+        ans = 0
+        for a, b, c in combinations(nums, 3):
+            if (a < b+c and a < b+c) and\
+               (b < c+a and b < c+a) and\
+               (c < a+b and c < a+b):
+                ans += 1
+
+        return ans
+
+
+test = Solution()
+test.triangleNumber([2,2,3,4])
