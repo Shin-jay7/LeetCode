599e7c363f936a1f7cef7eb44084c32c2304bb2d
Shin Ikeji <shin@jay7.net>
2023-01-07 09:18:36 -0800

    Two solutions

--- 0628_maximum_product_of_three_numbers.py
+++ 0628_maximum_product_of_three_numbers.py
@@ -1,8 +1,17 @@
 from __future__ import annotations
 from typing import List
+from heapq import nlargest, nsmallest
 
 
 class Solution:
     def maximumProduct(self, nums: List[int]) -> int:
         nums.sort()
         return max(nums[0] * nums[1] * nums[-1], nums[-1] * nums[-2] * nums[-3])
+
+
+class Solution:
+    def maximumProduct(self, nums: List[int]) -> int:
+        small, large = nsmallest(2, nums), nlargest(3, nums)
+        return max(
+             small[0] * small[1] * large[0], large[0] * large[1] * large[2]
+            )
