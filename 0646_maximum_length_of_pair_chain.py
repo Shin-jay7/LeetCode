cc25f97edd99d9cb47403e08f59aaa96ed027b9e
Shin Ikeji <shin@jay7.net>
2023-01-16 13:25:45 -0800

    Sort pairs by x[1]

---
+++ 0646_maximum_length_of_pair_chain.py
@@ -0,0 +1,11 @@
+from __future__ import annotations
+from typing import List
+
+
+class Solution:
+    def findLongestChain(self, pairs: List[List[int]]) -> int:
+        cur, ans = float('-inf'), 0
+        for pair in sorted(pairs, key=lambda x: x[1]):
+            if cur < pair[0]:
+                cur, ans = pair[1], ans+1
+        return ans
