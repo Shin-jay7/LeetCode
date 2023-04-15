47a8b8b1b48534eae7a06965fcb24d121c366563
Shin Ikeji <shin@jay7.net>
2023-04-14 20:08:52 -0700

    Two solutions

--- 0685_redundant_connection2.py
+++ 0685_redundant_connection2.py
@@ -2,6 +2,34 @@ from __future__ import annotations
 from typing import List
 
 
+# https://leetcode.com/problems/redundant-connection-ii/solutions/549772/17-line-clean-python-solution/?orderBy=most_votes
+class Solution:
+    def findRedundantDirectedConnection(self, edges: List[List[int]]) \
+     -> List[int]:
+        n = len(edges)
+        child_parent = dict(zip(range(1, n+1), range(1, n+1)))
+        last_cycle_edge, candidate_edge, candidate = None, None, None
+
+        def root(x):
+            while child_parent[x] != x:
+                x = child_parent[x]
+            return x
+
+        for u, v in edges:
+            # two parents
+            if child_parent[v] != v:
+                candidate_edge, candidate = [u, v], v
+                continue
+            # cycle
+            if root(u) == v:
+                last_cycle_edge = [u, v]
+                continue
+            child_parent[v], n = u, n-1
+        return [child_parent[candidate], candidate] if n > 1\
+            else (candidate_edge or last_cycle_edge)
+
+
+
 # https://leetcode.com/problems/redundant-connection-ii/solutions/254733/python-union-find-clear-logic/?orderBy=most_votes
 class DisjointSet:  # UnionFind
     def __init__(self, n: int):
