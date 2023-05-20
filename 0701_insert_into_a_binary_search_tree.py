f8f1b7f40cfcdda481c00e28d93f7dab1febf39b
Shin Ikeji <shin@jay7.net>
2023-05-21 07:25:19 +0900

    Create a new node with val if not node

---
+++ 0701_insert_into_a_binary_search_tree.py
@@ -0,0 +1,23 @@
+from __future__ import annotations
+from typing import Optional
+
+
+class TreeNode:
+    def __init__(self, val: int = 0, left: Optional[TreeNode] = None,
+                 right: Optional[TreeNode] = None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def insertIntoBST(self,
+                      root: Optional[TreeNode],
+                      val: int) -> Optional[TreeNode]:
+        if not root:
+            return TreeNode(val)
+        if root.val > val:
+            root.left = self.insertIntoBST(root.left, val)
+        else:
+            root.right = self.insertIntoBST(root.right, val)
+        return root
