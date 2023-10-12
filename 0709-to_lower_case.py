95cfc8d6da30698998b96b671ecaa83a44eaf330
Shin-jay7 <shin@jay7.net>
2023-10-11 19:23:03 -0700

    First stub

---
+++ 0709-to_lower_case.py
@@ -0,0 +1,20 @@
+from __future__ import annotations
+
+
+class Solution:
+    def toLowerCase(self, s: str) -> str:
+        # print(s.lower())
+        return s.lower()
+    
+
+test = Solution()
+test.toLowerCase("Hello") # "hello"
+
+test = Solution()
+test.toLowerCase("here") # "here"
+
+test = Solution()
+test.toLowerCase("LOVELY") # "lovely"
+
+test = Solution()
+test.toLowerCase("al&phaBET") # "al&phabet"
